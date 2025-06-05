027652f8078de28a8444c4453072932c239e0e78
Aradhya <aradhya.trivedi@coditas.com>
2025-06-05 10:13:15 +0530

    Updated

--- src/admin-app/authentication/handlers/createUserHandler.go
+++ src/admin-app/authentication/handlers/createUserHandler.go
@@ -12,6 +12,15 @@ type CreateUserController struct {
 	service *business.CreateUserService
 }
 
+// @Summary Create new user
+// @Tags Authentication
+// @Accept json
+// @Produce json
+// @Param user body models.BFFCreateUserRequest true "User object"
+// @Success 200 {object} models.BFFCreateUserResponse
+// @Failure 400 {object} models.ErrorResponse
+// @Failure 500 {object} models.ErrorResponse
+// @Router /createUser [post]
 func NewCreateUserController(service *business.CreateUserService) *CreateUserController {
 	return &CreateUserController{
 		service: service,
@@ -25,10 +34,20 @@ func (controller *CreateUserController) CreateUserHandler(c *gin.Context) {
 		return
 	}
 
+	if err := c.BindJSON(&bffCreateUserRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	err := controller.service.CreateNewUser(c, c, bffCreateUserRequest)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	c.JSON(http.StatusInternalServerError, models.ErrorAPIResponse{
+		Error: "User Creation Failed Error",
+	})
+	return
+
 	c.JSON(http.StatusOK, gin.H{"message": "User created successfully"})
 }
